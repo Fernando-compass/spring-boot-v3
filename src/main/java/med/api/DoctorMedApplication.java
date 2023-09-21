f97af9752b65f330ae170f26593a5be93c65e9d1
Fernando-compass <138720471+Fernando-compass@users.noreply.github.com>
2023-09-21 09:13:56 -0300

    first commit

---
+++ src/main/java/med/api/DoctorMedApplication.java
@@ -0,0 +1,18 @@
+package med.api;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class DoctorMedApplication {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder()
+				.sources(DoctorMedApplication.class)
+				.bannerMode(Banner.Mode.OFF)
+				.run(args);
+	}
+
+}
